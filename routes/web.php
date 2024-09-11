e4cabb54e1b023129345c1416a7f1197d937589f
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-09-11 14:34:04 +0900

    topページのルーティングを実装  #3

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\TopController::class, 'index']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
