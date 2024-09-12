409fceac80b110f1992da6c02eb6fdb1aa7d902d
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-09-12 15:10:51 +0900

    詳細ページの表示のト州

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,1 +1,16 @@
-<p>ここは詳細ページです</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>詳細ページ</h1>
+</body>
+    @if(isset($item) && isset($user))
+        <p>{{$user->name}}</p>
+        <h2>{{$item->title}}</h2>
+        <span>{{$tags->[$item->tag]}}</span>
+    @endif
+</html>
