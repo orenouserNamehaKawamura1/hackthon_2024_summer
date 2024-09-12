8f499fa96760aa7cb82a23a156189bb4f28137c5
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-09-12 10:49:43 +0900

    postのmodelを作成 #8

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class TopController extends Controller
 {
     public function index(){
-        return view("top");
+        $items = Post::all();
+        return view("top",["items" => $items]);
     }
 }
