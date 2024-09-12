006c58a3ce2961b9790db817061b2952886af86c
h.motodate <h.motodate.sys23@morijyobi.ac.jp>
2024-09-12 13:48:29 +0900

    サイドバー作成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,1 +1,45 @@
-<p>トップページです</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/css/top-style.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="side flex">
+        <div class="sidebar">
+            <h1>Ideus</h1>
+            <div class="side_content">
+                <div class="flex search">
+                    <div class="img">
+                        <img src="/img/sagasu.png" alt="" width="100%">
+                    </div>
+                    <p>さがす</p>
+                </div>
+                <p id="login_p1">ログインしてideaをシェアしよう</p>
+                <p id="login_p2"> ログインすると投稿やいいねなどの機能を利用できます</p>
+                <p></p>
+                <div class="flex login">
+                    <div class="img">
+                        <img src="/img/login.png" alt="" width="100%">
+                    </div>
+
+                    <p>ログイン</p>
+                </div>
+
+
+            </div>
+        </div>
+        <div>
+            <div class="main_search">
+                <input type="text">
+                <input type="submit">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
