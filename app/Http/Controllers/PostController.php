ac4f71c07b18009089b7222be615ae1573b6115b
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2024-09-17 11:08:20 +0900

    ログインしているユーザーのidを登録できるように修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+
 
 class PostController extends Controller
 {
@@ -18,6 +20,7 @@ class PostController extends Controller
     // 投稿されたデータをデータベースに登録する関数
     public function addPost(Request $request){
         // 登録するデータを受け取る
+        $id = Auth::id();
         $title = $request->title;
         $tag = $request->tag;
         $file = $request->file('image');
@@ -31,7 +34,7 @@ class PostController extends Controller
 
         // 登録するデータの連想配列
         $param = [
-            "user_id" => 1,
+            "user_id" => $id,
             "tag_id" => $tag,
             "title" => $title,
             "description" => $description,
