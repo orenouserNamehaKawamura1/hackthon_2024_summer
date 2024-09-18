bbc70fe429f0f1ca0560d4fb2546080772d562c6
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-09-18 11:21:16 +0900

    コメント機能の実装 #32 close

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -11,8 +11,18 @@
     @if(isset($item) && isset($user))
         <p>{{$user->name}}</p>
         <h2>{{$item->title}}</h2>
+        <img src="{{asset('/storage/'.$item->img_path)}}" alt="">
         <span>{{$tag->name}}</span>
         <p>{{$item->description}}</p>
         <p>{{$item->created_at->format('Y/m/d')}}</p>
+        <div>
+            <h3>コメント</h3>
+            <form action="/comment" method="post">
+            @csrf
+                <input type="text" name="comment">
+                <input type="number" name="detailId" hidden value="{{$item->id}}">
+                <input type="submit" value="コメント">
+            </form>
+        </div>
     @endif
 </html>
