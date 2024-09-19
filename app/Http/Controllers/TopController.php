76ce234f1feb0a272aafc514df0971004ca7b078
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2024-09-19 11:45:20 +0900

    一覧の修正

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 
 class TopController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         // if (Auth::check()) {
         //     // ログインしているユーザーはリダイレクト
         //     return redirect('/top');
@@ -16,8 +16,15 @@ class TopController extends Controller
         //     // 未ログインのユーザーにはトップページを表示
         //     return view('top');
         // }
-        $items = Post::all()->sortByDesc("created_at");
-        return view("top",["items" => $items]);
+        $id = $request->input('tab');
+        if(isset($id)) {
+            //取得してきたidをもとにタグごとの一覧表示
+            $items = Post::where('tag_id',$id)->get();
+            return view("top",["items" => $items]);
+        } else {
+            $items = Post::orderBy('created_at', 'desc')->get();
+            return view("top",["items" => $items]);
+        }
     }
 
     // public function top() {
