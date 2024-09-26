ae76205be55fc09d42ccb0a20639a48f85009288
Koyama Hinata <h.koyama.sys23@morijyobi.ac.jp>
2024-09-26 20:38:42 +0900

    フォントつけたのとサイドバー修正

--- resources/views/layouts/sideber.blade.php
+++ resources/views/layouts/sideber.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     <div class="side_title">
-        <h1>Ideus</h1>
+        <h1 class="montserrat-header">Ideus</h1>
         @if(Auth::check())
         <p class="midiumtext"> {{Auth::user()->name}}</p>
         @endif
@@ -11,13 +11,14 @@
         <div>
             @if(Auth::check())
             <div class="side_outside">
-                <div class="flex side_text">
 
+                <div class="flex side_text">
                     <div class="img">
                         <img src="/img/sagasu.png" alt="" width="90%">
                     </div>
                     <a href="/">さがす </a>
                 </div>
+
                 <div class="flex myPage side_text">
                     <div class="img">
                         <img src="/img/mypage.png" alt="" width="90%">
@@ -39,7 +40,7 @@
             </div>
 
             @else
-            <div class="login side_text">
+            <div class="side_outside">
 
                 <div class="flex side_text">
                     <div class="img">
@@ -47,9 +48,11 @@
                     </div>
                     <a href="/">さがす</a>
                 </div>
+
                 <p id="login_p1">ログインしてideaをシェアしよう</p>
                 <p id="login_p2"> ログインすると投稿やいいねなどの機能を利用できます</p>
-                <div class="flex">
+
+                <div class="flex login side_text">
                     <div class="img">
                         <img src="/img/login.png" alt="" width="90%">
                     </div>
