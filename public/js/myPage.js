4201a2886bfb28b247a3ac18d42a75764cd47fd6
h.motodate <h.motodate.sys23@morijyobi.ac.jp>
2024-09-26 11:31:23 +0900

    mypageの作成

---
+++ public/js/myPage.js
@@ -0,0 +1,53 @@
+"use strict";
+"use strict";
+const tagColor = ["#E0E62F", "#E3DB2F", "#E8CB2E", "#F0B22D", "#F69E2D", "#FC872C", "#F56551", "#F84A75"];
+const changeColor = ["#E0E62F", "#E3DB2F", "#E8CB2E", "#F0B22D", "#F69E2D", "#FC872C", "#F56551", "#F84A75"];
+const tagLabel = document.getElementsByClassName("tab-label");
+const tabContent = document.getElementsByClassName("tab-content");
+// 現在選択されている要素番号を格納する変数
+let nowIndex = 0;
+for (let i = 0; i < tagLabel.length; i++) {
+    // 色を変える関数に要素番号を渡す
+    tagLabel[i].addEventListener("click", { index: i, handleEvent: changeTabColor });
+}
+
+// tabの色を変える関数
+function changeTabColor() {
+    // 要素番号を受け取る
+    const index = this.index;
+    // 同じタブが押された場合は処理を実行しない
+    if (nowIndex === index) {
+        return;
+    }
+    // 選択された要素のスタイルを変更
+    tagLabel[index].style.cssText = `background-color: ${changeColor[index]}; border: 2px solid ${changeColor[index]}; padding:3px; margin-top:6px`;
+    // 選択されたコンテンツのボーダーを変更
+    tabContent[index].style.cssText = `border: 2px solid ${changeColor[index]}; `;
+    // 元々選択されていた色を元の色に変更
+    tagLabel[nowIndex].style.cssText = `background-color: ${tagColor[nowIndex]}; border:none;`;
+    // 元々選択されていたコンテンツのボーダーを削除
+    tabContent[nowIndex].style.cssText = `border: none`;
+    // 表示したコンテンツに表示するクラスを付与
+    tabContent[index].classList.add("tab-content-on");
+    // 元々表示されていたコンテンツの表示するクラスを削除
+    tabContent[nowIndex].classList.remove("tab-content-on");
+    // 現在選択されている要素を変更
+    nowIndex = index;
+}
+// ページ表示時に実行する処理
+function init() {
+    // 一覧表示(tab1)に必要なスタイルを適用
+    // 選択された要素のスタイルを変更
+    tagLabel[0].style.cssText = `background-color: ${changeColor[0]}; border: 2px solid ${changeColor[0]}`;
+    // 選択されたコンテンツのボーダーを変更
+    tabContent[0].style.cssText = `border: 2px solid ${changeColor[0]}`;
+    // 表示したコンテンツに表示するクラスを付与
+    tabContent[0].classList.add("tab-content-on");
+
+    for (let i = 0; i < tagLabel.length; i++) {
+        // タブに背景色を付ける
+        tagLabel[i].style.cssText = `background-color: ${tagColor[i]}`;
+    }
+}
+
+init();
--- resources/views/myPage.blade.php
+++ resources/views/myPage.blade.php
@@ -1,88 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="{{asset('/css/all-style.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/all-style.css')}}">
-
+    <link rel="stylesheet" href="{{asset('/css/top-style.css')}}">
 </head>
-<body>
-    <h1>ここはmypageです</h1>
-    @component("layouts.sideber")
-    @endcomponent
-<h2>投稿一覧</h2>
 
-<div class="tab-content">
-    @if(isset($posts))
-        @if(count($posts))
-            @foreach($posts as $item)
-            <div class="post">
-                <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
-                <p id="post_p_1">{{$item->description}}</p>
-                <div class="post_p_2 flex">
-                    <p>{{$item->user->name}}</p>
-                    <img src="/img/hart.png" width="2.5%" height="2.5%">
-                    <p>{{$item->good}}</p>
-                </div>
-                <a href="/edit/{{$item->id}}">編集する</a>
-            </div>
-            @endforeach
-            <div class = "page">
-            {{$posts -> links('vendor.pagination.bootstrap-4')}}
-        </div>
-        @else
-            <p>Not data</p>    
-        @endif
-    @endif
-</div>
-<div class="tab-content">
-    @if(isset($goods))
-        @if(count($goods))
-            @foreach($goods as $item)
-            <div class="post">
-                <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
-                <p id="post_p_1">{{$item->description}}</p>
-                <div class="post_p_2 flex">
-                    <p>{{$item->user->name}}</p>
-                    <img src="/img/hart.png" width="2.5%" height="2.5%">
-                    <p>{{$item->good}}</p>
-                </div>
-            </div>
-            @endforeach
-            <div class = "page">
-            {{$goods -> links('vendor.pagination.bootstrap-4')}}
-        </div>
-        @else
-            <p>Not data</p>    
-        @endif
-    @endif
-</div>
-<div class="tab-content">
-    @if(isset($comments))
-        @if(count($comments))
-            @foreach($comments as $item)
-            <div class="post">
-                <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
-                <p id="post_p_1">{{$item->description}}</p>
-                <div class="post_p_2 flex">
-                    <p>{{$item->user->name}}</p>
-                    <img src="/img/hart.png" width="2.5%" height="2.5%">
-                    <p>{{$item->good}}</p>
-                </div>
-            </div>
-            @endforeach
-            <div class = "page">
-            {{$comments -> links('vendor.pagination.bootstrap-4')}}
-        </div>
-        @else
-            <p>Not data</p>    
-        @endif
-    @endif
-</div>
+<body id="body">
+    <div class="side flex" id="myPage">
+        @component("layouts.sideber")
+        @endcomponent
+        <div class="main">
 
 
+            @foreach($comments as $comment)
+            <h2>{{$comment->user->name}}</h2>
+            @endforeach
+            <h3>プロフィール</h3>
+
+            <div>
+                <div class="tab-wrap">
+                    <input id="tab01" type="radio" name="tab" class="tab-switch tab1" checked="checked">
+                    <label class="tab-label" for="tab01">
+                        投稿
+                    </label>
+                    <div class="tab-content tab-content-on">
+                        @if(isset($posts))
+                        @if(count($posts))
+                        @foreach($posts as $item)
+                        <div class="post">
+                            <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
+                            <p id="post_p_1">{{$item->description}}</p>
+                            <div class="post_p_2 flex">
+                                <p>{{$item->user->name}}</p>
+                                <img src="/img/hart.png" width="2.5%" height="2.5%">
+                                <p>{{$item->good}}</p>
+                            </div>
+                            <a href="/edit/{{$item->id}}">編集する</a>
+                        </div>
+                        @endforeach
+                        <div class="page">
+                            {{$posts -> links('vendor.pagination.bootstrap-4')}}
+                        </div>
+                        @else
+                        <p>Not data</p>
+                        @endif
+                        @endif
+                    </div>
+                    <input id="tab02" type="radio" name="tab" class="tab-switch tab2">
+                    <label class="tab-label" for="tab02">
+                        いいね
+                    </label>
+                    <div class="tab-content">
+                        @if(isset($goods))
+                        @if(count($goods))
+                        @foreach($goods as $item)
+                        <div class="post">
+                            <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
+                            <p id="post_p_1">{{$item->description}}</p>
+                            <div class="post_p_2 flex">
+                                <p>{{$item->user->name}}</p>
+                                <img src="/img/hart.png" width="2.5%" height="2.5%">
+                                <p>{{$item->good}}</p>
+                            </div>
+                        </div>
+                        @endforeach
+                        <div class="page">
+                            {{$goods -> links('vendor.pagination.bootstrap-4')}}
+                        </div>
+                        @else
+                        <p>Not data</p>
+                        @endif
+                        @endif
+                    </div>
+                    <input id="tab03" type="radio" name="tab" class="tab-switch tab3">
+                    <label class="tab-label" for="tab03">
+                        コメント
+                    </label>
+                    <div class="tab-content">
+                        @if(isset($comments))
+                        @if(count($comments))
+                        @foreach($comments as $item)
+                        <div class="post">
+                            <a href="{{route('detail',['id' => $item->id])}}" class="post_a">{{$item->title}}</a>
+                            <p id="post_p_1">{{$item->description}}</p>
+                            <div class="post_p_2 flex">
+                                <p>{{$item->user->name}}</p>
+                                <img src="/img/hart.png" width="2.5%" height="2.5%">
+                                <p>{{$item->good}}</p>
+                            </div>
+                        </div>
+                        @endforeach
+                        <div class="page">
+                            {{$comments -> links('vendor.pagination.bootstrap-4')}}
+                        </div>
+                        @else
+                        <p>Not data</p>
+                        @endif
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </body>
+<script src="/js/myPage.js"></script>
 
 </html>
